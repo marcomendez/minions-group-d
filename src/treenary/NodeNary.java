475b60b5014a8592ab51ccab24dd59e23eaa208d
marcomendez <marco.mendez@communitybrands.com>
2019-06-28 20:50:05 -0400

    done

---
+++ src/treenary/NodeNary.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+import java.util.List;
+
+/***
+ * Created By Marco Mendez.
+ */
+public class NodeNary {
+
+    public String value;
+    public List<NodeNary> Children;
+
+    /**
+     * Constructor.
+     * @param value - value.
+     */
+    public NodeNary(String value) {
+        this.value = value;
+        Children = new ArrayList<>();
+    }
+}
